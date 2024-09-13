cec8e6a72236164c9392a238e8c5f3bcee04c8ef
Tyler Caprioli <capriolityler@gmail.com>
2024-09-13 16:58:12 -0400

    Implement new route handlers

--- api/server.go
+++ api/server.go
@@ -1,32 +1,27 @@
 package api
 
 import (
-	"encoding/json"
+	"context"
 	"net/http"
 
 	db "www.github.com/TCaprioli/Apptrack-go/db/sqlc"
 )
 
 type ApiServer struct {
-	addr string
+	addr  string
 	store *db.Store
+	ctx   context.Context
 }
 
-func NewServer(addr string, store *db.Store) *ApiServer {
-	return &ApiServer{addr: addr, store: store}
+func NewServer(addr string, store *db.Store, ctx context.Context) *ApiServer {
+	return &ApiServer{addr: addr, store: store, ctx: ctx}
 }
 
 func (s ApiServer) Run() error {
 	router := http.NewServeMux()
-	// initialize root route
-	// TODO: Remove after adding handlers
-	router.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
-		resp:= struct{ Message string `json:"message"`}{Message: "Hello, world"}
-		w.Header().Set("Content-Type", "application/json")
-		if err := json.NewEncoder(w).Encode(resp); err != nil {
-			http.Error(w,"failed to encode resp", http.StatusInternalServerError)
-		}
-	})
+	router.Handle("/users/", userHandler(s.store, s.ctx))
+	router.Handle("/applications", authMiddleware(applicationHandler(s.store, s.ctx)))
+
 	server := http.Server{Addr: s.addr, Handler: router}
 	return server.ListenAndServe()
 }
--- main.go
+++ main.go
@@ -1,24 +1,28 @@
 package main
 
 import (
+	"context"
 	"log"
 
 	_ "github.com/lib/pq"
 	"www.github.com/TCaprioli/Apptrack-go/api"
 	db "www.github.com/TCaprioli/Apptrack-go/db/sqlc"
 )
+
 var addr = ":8080"
+
 func main() {
-	conn, connErr:= db.Connect()
+	dbCtx := context.Background()
+	conn, connErr := db.Connect()
 	if connErr != nil {
 		log.Fatalf("Failed to connect to the database. %v", connErr)
 	}
 	log.Print("Connected to the database...")
 	store := db.NewStore(conn)
-	server := api.NewServer(addr, store)
+	server := api.NewServer(addr, store, dbCtx)
 	log.Printf("Server starting at address %v", addr)
-	if serverErr := server.Run();serverErr != nil {
+	if serverErr := server.Run(); serverErr != nil {
 		log.Fatal(serverErr)
-	} 
-	
+	}
+
 }
