aa1d107a02ac586419d9dee4e2de71d32b117169
Tyler Caprioli <capriolityler@gmail.com>
2024-09-13 18:53:53 -0400

    Revert env util

--- utils/utils.go
+++ utils/utils.go
@@ -2,13 +2,16 @@ package utils
 
 import (
 	"log"
+	"os"
 
 	"github.com/joho/godotenv"
 )
 
 func LoadEnv() {
-	err := godotenv.Load()
-	if err != nil {
-		log.Println("Warning: Failed to load .env file. Using existing environment variables.")
+	if os.Getenv("RAILWAY_ENVIRONMENT") == "" {
+		err := godotenv.Load()
+		if err != nil {
+			log.Println("Warning: Failed to load .env file. Using existing environment variables.")
+		}
 	}
 }
