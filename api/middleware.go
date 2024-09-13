c7072c0a428ee5ba5821255277f1f54131b8a983
Tyler Caprioli <capriolityler@gmail.com>
2024-09-13 16:57:45 -0400

    Add auth middleware

---
+++ api/middleware.go
@@ -0,0 +1,60 @@
+package api
+
+import (
+	"context"
+	"log"
+	"net/http"
+	"os"
+	"strings"
+
+	"github.com/joho/godotenv"
+	"github.com/o1egl/paseto"
+)
+
+var authorizationTypeBearer = "bearer"
+
+type contextKey string
+
+const UserKey contextKey = "user"
+
+type UserContext struct {
+	id int32
+}
+
+func authMiddleware(next http.Handler) http.Handler {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	symmetricKey := os.Getenv("SECRET_KEY")
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		header := r.Header.Get("Authorization")
+		if header == "" {
+			http.Error(w, "Unauthorized", http.StatusUnauthorized)
+			return
+		}
+		fields := strings.Fields(header)
+		if len(fields) < 2 {
+			http.Error(w, "invalid authorization header format", http.StatusUnauthorized)
+			return
+		}
+
+		authorizationType := strings.ToLower(fields[0])
+		if authorizationType != authorizationTypeBearer {
+			http.Error(w, "unsupported authorization type", http.StatusUnauthorized)
+			return
+		}
+
+		token := fields[1]
+		var user UserContext
+		err := paseto.NewV2().Decrypt(token, []byte(symmetricKey), &user.id, nil)
+		if err != nil {
+			http.Error(w, "Invalid token", http.StatusUnauthorized)
+			return
+		}
+		ctx := r.Context()
+		ctx = context.WithValue(ctx, UserKey, user)
+		next.ServeHTTP(w, r.WithContext(ctx))
+	})
+}
