acb6a459ee471d2f89c9775a8c5e939953df6b3e
Tyler <capriolityler@gmail.com>
2024-10-09 20:01:12 -0400

    Update local dev environment

--- db/sqlc/connect.go
+++ db/sqlc/connect.go
@@ -15,7 +15,6 @@ func Connect()(*sql.DB, error) {
 		return sql.Open("postgres", databaseURL)
 	} else {
 		user := os.Getenv("POSTGRES_USER")
-		host := os.Getenv("POSTGRES_HOST")
 		password := os.Getenv("POSTGRES_PASSWORD")
 		name := os.Getenv("POSTGRES_NAME")
 		portStr := os.Getenv("POSTGRES_PORT")
@@ -23,9 +22,9 @@ func Connect()(*sql.DB, error) {
 		if err != nil {
 			return nil, fmt.Errorf("invalid port number: %v", err)
 		}
-		psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
+		psqlInfo := fmt.Sprintf("port=%d user=%s "+
 			"password=%s dbname=%s sslmode=disable",
-			host, port, user, password, name)
+			port, user, password, name)
 		return sql.Open("postgres", psqlInfo)
 	}
 
